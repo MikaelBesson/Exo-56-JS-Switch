f332efa15a38bf926a64ef19c8492836841b7ff9
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-11-25 14:31:21 +0100

    exo 56

---
+++ script.js
@@ -0,0 +1,25 @@
+let accesoire = prompt("votre accesoire?");
+
+
+switch (accesoire) {
+    case "souris":
+        alert("j'utilise une "+ accesoire);
+        break;
+    case "clavier":
+        alert("j'utilise un "+ accesoire);
+        break;
+    case "ordinateur":
+        alert("j'utilise un "+ accesoire);
+        break;
+    case "tapis":
+        alert("j'utilise un "+ accesoire);
+        break;
+    case "imprimante":
+        alert("j'utilise une "+ accesoire);
+        break;
+    case "ondulateur":
+        alert("j'utilise un "+ accesoire);
+        break;
+    default:
+        alert("materiel inconnu");
+}
